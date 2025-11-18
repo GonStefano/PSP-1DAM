cc50eedbbf224fe918e4d75d5cc5ecaf97ab044f
GonStefano <156598398+GonStefano@users.noreply.github.com>
2025-11-18 08:52:15 +0100

    Add files via upload

---
+++ Hilos/Sala_Ejecutar.java
@@ -0,0 +1,66 @@
+class Salon{
+    private int numero;
+    private int aforoMax=5;
+
+    public synchronized void entrar(int n){
+        
+        while (numero==aforoMax) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
+
+        numero++;
+        System.out.println("Cliente "+n+" entra a la sala de entrenamiento");
+
+        notifyAll();
+    }
+
+
+    public synchronized void salir(int n){
+
+        while (numero==0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
+        numero--;
+        System.out.println("Cliente "+n+" saliendo de la sala");
+        notifyAll();
+    }
+}
+
+class Cliente extends Thread{
+    int id;
+    Salon c;
+
+    public Cliente(Salon c,int id){
+        this.id = id;
+        this.c = c;
+    }
+
+    public void run(){
+        c.entrar(id);
+        try {
+            Thread.sleep((long) (Math.random() * 3000 + 1000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        c.salir(id);
+    }
+}
+
+public class Sala_Ejecutar {
+    public static void main(String[] args) {
+        Thread clientes[] = new Thread[20];
+        Salon c = new Salon();
+
+        for (int i = 0; i < clientes.length; i++) {
+            clientes[i] = new Cliente(c, i+1);
+
+            clientes[i].start();
+        }
+    }
+}
